1f3e37dab438c24f6de024b9942efe88ea7bae41
SarthakN <sarthaknayyar6@gmail.com>
2024-07-09 20:51:41 +0530

    cors upd

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ connectDB(MONGO_URI);
 
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://admirable-quokka-c4bf0c.netlify.app"], // Add your frontend domain here
     credentials: true
 }));
 
